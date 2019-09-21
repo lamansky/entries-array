31efcc2e1663b7494225eb164d947e91a17121a0
Fr. John Lamansky <lamansky@users.noreply.github.com>
2019-09-21 07:33:01 +0200

    Use assert.deepStrictEqual

--- test.js
+++ test.js
@@ -2,11 +2,10 @@
 
 const assert = require('assert')
 const entries = require('.')
-const equals = require('equals')
 
 describe('entriesArray()', function () {
   it('should return Array entries', function () {
-    assert(equals(entries(['a', 'b']), [[0, 'a'], [1, 'b']]))
+    assert.deepStrictEqual(entries(['a', 'b']), [[0, 'a'], [1, 'b']])
   })
 
   it('should return iterator entries', function () {
@@ -15,56 +14,56 @@ describe('entriesArray()', function () {
       yield 'b'
     }
 
-    assert(equals(entries(gen()), [[0, 'a'], [1, 'b']]))
+    assert.deepStrictEqual(entries(gen()), [[0, 'a'], [1, 'b']])
   })
 
   it('should return Map entries', function () {
-    assert(equals(entries(new Map([['key', 'value']])), [['key', 'value']]))
+    assert.deepStrictEqual(entries(new Map([['key', 'value']])), [['key', 'value']])
   })
 
   it('should return entries of a custom Map class', function () {
     class MyMap {
       entries () { return [['key', 'value']] }
     }
-    assert(equals(entries(new MyMap()), []))
-    assert(equals(entries(new MyMap(), {maps: MyMap}), [['key', 'value']]))
+    assert.deepStrictEqual(entries(new MyMap()), [])
+    assert.deepStrictEqual(entries(new MyMap(), {maps: MyMap}), [['key', 'value']])
   })
 
   it('should return entries of a custom Map class referenced by name string', function () {
     class MyMap {
       entries () { return [['key', 'value']] }
     }
-    assert(equals(entries(new MyMap()), []))
-    assert(equals(entries(new MyMap(), {maps: 'MyMap'}), [['key', 'value']]))
+    assert.deepStrictEqual(entries(new MyMap()), [])
+    assert.deepStrictEqual(entries(new MyMap(), {maps: 'MyMap'}), [['key', 'value']])
   })
 
   it('should return Object entries', function () {
-    assert(equals(entries({key: 'value'}), [['key', 'value']]))
+    assert.deepStrictEqual(entries({key: 'value'}), [['key', 'value']])
   })
 
   it('should return inherited Object properties if `inObj` is true', function () {
     function A () {}
     A.prototype.key = 'value'
-    assert(equals(entries(new A(), {inObj: true}), [['key', 'value']]))
+    assert.deepStrictEqual(entries(new A(), {inObj: true}), [['key', 'value']])
   })
 
   it('should return non-enumerable Object properties if `reflectObj` is true', function () {
     const obj = {}
     Object.defineProperty(obj, 'key', {value: 'value', enumerable: false})
     assert.strictEqual(entries(obj).length, 0)
-    assert(equals(entries(obj, {reflectObj: true}), [['key', 'value']]))
+    assert.deepStrictEqual(entries(obj, {reflectObj: true}), [['key', 'value']])
   })
 
   it('should return Set entries', function () {
-    assert(equals(entries(new Set(['value'])), [[0, 'value']]))
+    assert.deepStrictEqual(entries(new Set(['value'])), [[0, 'value']])
   })
 
   it('should return String characters', function () {
-    assert(equals(entries('hi'), [[0, 'h'], [1, 'i']]))
+    assert.deepStrictEqual(entries('hi'), [[0, 'h'], [1, 'i']])
   })
 
   it('should return Typed Array entries', function () {
-    assert(equals(entries(new Int32Array(new ArrayBuffer(4))), [[0, 0]]))
+    assert.deepStrictEqual(entries(new Int32Array(new ArrayBuffer(4))), [[0, 0]])
   })
 
   it('should return an empty array for undefined', function () {
@@ -84,11 +83,11 @@ describe('entriesArray()', function () {
   })
 
   it('should return entries in reverse order if `reverse` is true', function () {
-    assert(!equals(entries(['a', 'b'], {reverse: false}), [[1, 'b'], [0, 'a']]))
-    assert(equals(entries(['a', 'b'], {reverse: true}), [[1, 'b'], [0, 'a']]))
+    assert.notDeepStrictEqual(entries(['a', 'b'], {reverse: false}), [[1, 'b'], [0, 'a']])
+    assert.deepStrictEqual(entries(['a', 'b'], {reverse: true}), [[1, 'b'], [0, 'a']])
   })
 
   it('should support the bind operator', function () {
-    assert(equals(entries.call(['test']), [[0, 'test']]))
+    assert.deepStrictEqual(entries.call(['test']), [[0, 'test']])
   })
 })
